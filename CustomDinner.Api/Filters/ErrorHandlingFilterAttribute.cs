e023a6e00575f7d366e26afb1b724d624325669b
Gabriel Sousa <gabrielkf@gmail.com>
2023-04-30 13:05:00 -0300

    feat: extract constant string

--- CustomDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
+++ CustomDinner.Api/Filters/ErrorHandlingFilterAttribute.cs
@@ -5,11 +5,12 @@ namespace CustomDinner.Api.Filters;
 
 public class ErrorHandlingFilterAttribute : ExceptionFilterAttribute
 {
+    private const string InternalErrorType = "https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/500";
     public override void OnException(ExceptionContext context)
     {
         var problemDetails = new ProblemDetails
         {
-            Type = "https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Status/500",
+            Type = InternalErrorType,
             Instance = context.HttpContext.Request.Path,
             Status = StatusCodes.Status500InternalServerError,
             Detail = context.Exception.Message
