7f9b072658a29daeea6227e2da6716de302ef697
Gabriel Sousa <gabrielkf@gmail.com>
2023-07-15 15:40:58 -0300

    feat: add validation to behavior

--- CustomDinner.Application/Common/Behaviors/RegisterValidationBehavior.cs
+++ CustomDinner.Application/Common/Behaviors/RegisterValidationBehavior.cs
@@ -1,18 +1,40 @@
 using CustomDinner.Application.Authentication.Commands.Common;
 using CustomDinner.Application.Authentication.Commands.Register;
 using ErrorOr;
+using FluentValidation;
+using FluentValidation.Results;
 using MediatR;
+using Microsoft.AspNetCore.Authentication;
 
 namespace CustomDinner.Application.Common.Behaviors;
 
 public class RegisterValidationBehavior : IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>
 {
-    public async Task<ErrorOr<AuthenticationResult>> Handle(RegisterCommand request,
+    private readonly IValidator<RegisterCommand> _validator;
+
+    public RegisterValidationBehavior(IValidator<RegisterCommand> validator)
+    {
+        _validator = validator;
+    }
+
+    public async Task<ErrorOr<AuthenticationResult>> Handle(
+        RegisterCommand request,
         RequestHandlerDelegate<ErrorOr<AuthenticationResult>> next,
         CancellationToken cancellationToken)
     {
-        var result = await next();
+        var validationResult = await _validator.ValidateAsync(request, cancellationToken);
 
-        return result;
+        if (validationResult.IsValid)
+        {
+            return await next();
+        }
+
+        var errors = validationResult.Errors
+            .Select(failure => Error.Validation(
+                failure.PropertyName,
+                failure.ErrorMessage))
+            .ToList();
+
+        return errors;
     }
 }
