5210c4ac62a7a45fd88e767e78b47a1b9894de2c
Gabriel Sousa <gabrielkf@gmail.com>
2023-10-09 21:45:17 -0300

    fix(mapping): Authentication mapping

--- CustomDinner.Api/Mappers/AuthenticationMapping.cs
+++ CustomDinner.Api/Mappers/AuthenticationMapping.cs
@@ -11,6 +11,15 @@ public class AuthenticationMapping : IRegister
     public void Register(TypeAdapterConfig config)
     {
         config.NewConfig<AuthenticationResult, AuthenticationResponse>()
-            .Map(dest => dest, src => src.User);
+            .Map(dest => dest.Id,
+                src => src.User.Id.Value.ToString())
+            .Map(dest => dest.FirstName,
+                src => src.User.FirstName)
+            .Map(dest => dest.LastName,
+                src => src.User.LastName)
+            .Map(dest => dest.Email,
+                src => src.User.Email)
+            .Map(dest => dest.Token,
+                src => src.Token);
     }
 }
